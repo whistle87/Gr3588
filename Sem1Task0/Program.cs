e252ae57c0263736fd358ba3d32bb6ae6b2be98d
whistle87 <i379@yandex.ru>
2022-12-01 09:19:00 +0300

    Add commetns in Task 1

--- Sem1Task0/Program.cs
+++ Sem1Task0/Program.cs
@@ -1,9 +1,15 @@
-﻿string? inputLine = Console.ReadLine();
+﻿// считываем данные с консоли
+string? inputLine = Console.ReadLine(); 
 
+// Проверяем, что данные не пустые
 if(inputLine!=null)
 {
+    // Преобразыем введеное число из строковой переменной в целочисленную
     int inputNumber = int.Parse(inputLine);
     
+    // Находим квадрат числа
     int outResult = inputNumber*inputNumber;
+
+    // Выводим результат в консоль
     Console.WriteLine(outResult);
 }
