c6c02979575b8aaaf31a60bddb7f56a9895698fb
whistle87 <i379@yandex.ru>
2022-12-01 10:25:34 +0300

    Add task 5

---
+++ Sem1Task5/Program.cs
@@ -0,0 +1,21 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine!=null)
+{
+    // Преобразыем введеное число из строковой переменной в целочисленную
+    int inputNumber = int.Parse(inputLine);
+   
+    int startNumber = inputNumber*(-1);
+
+    string outLine = string.Empty;
+
+    while (startNumber<inputNumber)
+    {
+        outLine = outLine + startNumber + ", ";
+        startNumber++;
+    }
+
+    outLine = outLine+inputNumber;
+    // Выводим результат в консоль
+    Console.WriteLine(outLine);
+}
