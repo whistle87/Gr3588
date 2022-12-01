a1c68b5c33bef6f53d955771156196aacc300736
whistle87 <i379@yandex.ru>
2022-12-01 12:01:35 +0300

    Add task 6

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿// считываем данные с консоли
+string? inputLine = Console.ReadLine(); 
+
+// Проверяем, что данные не пустые
+if(inputLine!=null)
+{
+    // Проверяем остаток от деления на 2 и выводим результат
+    int inputNumber = int.Parse(inputLine);
+    if (inputNumber%2==0)
+    {
+        Console.WriteLine("число четное");
+    }
+    else
+    {
+        Console.WriteLine("число не четное");
+    }
+   
+}
