2d5976ad56e77feef252c5ad3d9d4b650bc03c1f
whistle87 <i379@yandex.ru>
2022-12-01 09:12:06 +0300

    Add task 0

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,9 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine!=null)
+{
+    int inputNumber = int.Parse(inputLine);
+    
+    int outResult = inputNumber*inputNumber;
+    Console.WriteLine(outResult);
+}
