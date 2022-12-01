4708fad3e85e6ed822e9de56b367ab114cef2a35
whistle87 <i379@yandex.ru>
2022-12-01 11:17:52 +0300

    Add task 2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// считываем данные с консоли
+string? input1Line = Console.ReadLine(); 
+string? input2Line = Console.ReadLine();
+
+// Проверяем, что данные не пустые
+if(input1Line!=null&&input2Line!=null)
+{
+    // Преобразыем введеное число из строковой переменной в целочисленную
+    int input1Number = int.Parse(input1Line);
+    int input2Number = int.Parse(input2Line);
+
+// Сравниваем числа и выводим результат в консоль
+    if (input1Number>input2Number)
+    {
+        Console.WriteLine(input1Number+" > "+input2Number);
+    }
+    else
+    {
+        Console.WriteLine(input2Number+" > "+input1Number);
+    }
+       
+}
