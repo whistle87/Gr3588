162af20a633b4c969f80706341cba0ab5453e0a1
whistle87 <i379@yandex.ru>
2022-12-04 14:47:40 +0300

    Add task 16

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,11 @@
+﻿void Test (int numFirst, int numSecond)
+{
+    bool result = (numFirst/numSecond==numSecond);
+    if(result==true)Console.WriteLine("Number "+numFirst+" is a square of "+numSecond);
+    else Console.WriteLine("Number "+numFirst+" is not a square of "+numSecond);
+}
+
+int number1 = int.Parse(Console.ReadLine()??"0");
+int number2 = int.Parse(Console.ReadLine()??"0");
+Test(number1,number2);
+Test(number2,number1);
