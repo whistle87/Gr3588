015e7660386e1fd75d5ed12b03371fb5b0238721
whistle87 <i379@yandex.ru>
2022-12-04 13:55:06 +0300

    Add task 12

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,11 @@
+﻿int firstNumber = int.Parse(Console.ReadLine()??"0");
+int secondNumber = int.Parse(Console.ReadLine()??"0");
+
+if(secondNumber%firstNumber == 0)
+{
+    Console.WriteLine(secondNumber+" is multiple "+firstNumber);
+}
+else
+{
+    Console.WriteLine(secondNumber+" is not multiple "+firstNumber+", remainde of division is "+secondNumber%firstNumber);
+}
