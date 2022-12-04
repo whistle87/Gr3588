bf437c992596920004565512a36c72a7dc325003
whistle87 <i379@yandex.ru>
2022-12-04 13:31:42 +0300

    Add task 11

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Random numberSintezator = new System.Random();
+
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+
+// int firstNumber = num/100;
+// int secondNumber = num%10;
+
+int result  = (num/100)*10+(num%10);
+
+Console.WriteLine(result);
