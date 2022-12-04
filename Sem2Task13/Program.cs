702d67a05dd1c96235e1a00e52bd0487ddcded06
whistle87 <i379@yandex.ru>
2022-12-04 17:32:37 +0300

    Add task 13

---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// считываем данные с консоли
+int inputNumber = int.Parse(Console.ReadLine()??"0");
+// проверяем корректность введеных данных
+if(inputNumber>99)
+{
+    // вычисляем количество цифры в введенном числе
+    int numberOfDigits = (int)Math.Round(Math.Log10((double)inputNumber));
+    // выводим 3-ю цифру числа
+    Console.WriteLine((inputNumber/(int)Math.Pow(10, (numberOfDigits-3)))%10);
+}
+else
+{
+    Console.WriteLine("There isn't third number");
+}
