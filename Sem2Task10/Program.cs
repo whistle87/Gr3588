29f6c9003c1f67a982436d8e01174e60d20a115d
whistle87 <i379@yandex.ru>
2022-12-04 17:34:49 +0300

    Add comment in task 10

--- Sem2Task10/Program.cs
+++ Sem2Task10/Program.cs
@@ -1,7 +1,9 @@
-﻿int inputNumber = int.Parse(Console.ReadLine()??"0");
-
+﻿// считываем данные с консоли
+int inputNumber = int.Parse(Console.ReadLine()??"0");
+// проверяем корректность введеных данныx
 if(inputNumber>99&&inputNumber<1000)
 {
+   // выводим вторую цифру
     Console.WriteLine("Second number: "+(inputNumber/10)%10);
 }
 else
