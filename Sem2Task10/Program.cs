0d9ce6eaa21d2909995fddf024b8ef7b0b1f72c1
whistle87 <i379@yandex.ru>
2022-12-04 15:04:30 +0300

    Add task 10

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿int inputNumber = int.Parse(Console.ReadLine()??"0");
+
+if(inputNumber>99&&inputNumber<1000)
+{
+    Console.WriteLine("Second number: "+(inputNumber/10)%10);
+}
+else
+{
+    Console.WriteLine("Number isn't correct, it should be from 100 to 999");
+}
