25ead668c4fcfcace86f1afd6c3498d22294fcb8
whistle87 <i379@yandex.ru>
2022-12-04 13:21:58 +0300

    Add task 9

---
+++ Sem2Task9/Program.cs
@@ -0,0 +1,29 @@
+﻿// create instance of the class randomazer
+System.Random numberSintezator = new System.Random();
+// // // get new randon number
+//  int number = numberSintezator.Next(10,100);
+// // //print number
+// // Console.WriteLine(number);
+// // //get first and second number separately
+//  int firstNumber = number/10;
+//  int secondNumber = number%10;
+// // // compare firs and second number and print max
+// if(firstNumber > secondNumber)
+// {
+//     Console.WriteLine(firstNumber);
+// }
+// else
+// {
+//     Console.WriteLine(secondNumber);
+// }
+
+//variant 2
+char[] digits = numberSintezator.Next(10,100).ToString().ToCharArray();
+
+Console.WriteLine(digits);
+
+int firstNumber = ((int)digits[0])-48;
+int secondNumber = ((int)digits[1])-48;
+
+int resultNumber = firstNumber>secondNumber?resultNumber=firstNumber:resultNumber=secondNumber;
+Console.WriteLine(resultNumber);
