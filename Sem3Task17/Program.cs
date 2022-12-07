24f5450a4a6e4db23c291d84b6f311bbf71b9bd7
whistle87 <i379@yandex.ru>
2022-12-07 11:21:28 +0300

    add task 17

---
+++ Sem3Task17/Program.cs
@@ -0,0 +1,23 @@
+﻿// // rading data
+// Console.WriteLine("Enter coordinate x");
+// int coordX = int.Parse(Console.ReadLine()??"0");
+// Console.WriteLine("Enter coordinate y");
+// int coordY = int.Parse(Console.ReadLine()??"0");
+
+int coordX = ReadData("Enter x coordinate");
+int coordY = ReadData("Enter y coordinate");
+PrintQuarterTest();
+// method defines a quarter by coordinate
+void PrintQuarterTest()
+ {
+ if (coordX>0&&coordY>0) Console.WriteLine("Point in 1 quarter");
+ if (coordX>0&&coordY<0) Console.WriteLine("Point in 2 quarter");
+ if (coordX<0&&coordY<0) Console.WriteLine("Point in 3 quarter");
+ if (coordX<0&&coordY>0) Console.WriteLine("Point in 4 quarter");
+ }
+//method read user's data
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
