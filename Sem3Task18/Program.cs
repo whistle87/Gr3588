5e441120cf3561b098eb1e05797b8bb2b0ed9a61
whistle87 <i379@yandex.ru>
2022-12-07 11:45:26 +0300

    Add task 18

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,20 @@
+﻿//method read user's data
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+// method defines a quarter by coordinate
+void PrintRange(int num)
+ {
+ if (num == 1) Console.WriteLine("x>0, y>0");
+ if (num == 2) Console.WriteLine("x>0, y<0");
+ if (num == 3) Console.WriteLine("x<0, y<0");
+ if (num == 4) Console.WriteLine("x<0, y>0");
+ }
+
+int quarter = ReadData("Enter a quarter");
+PrintRange(quarter);
+
+
