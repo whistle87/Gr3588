7c75b66861203967af9293fb88b8f487917ab834
whistle87 <i379@yandex.ru>
2022-12-08 14:39:50 +0300

    add task 21

---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,28 @@
+﻿//solution
+int[] pointA = ReadData("Enter coordinate point A in format x,y,z ");
+int[] pointB = ReadData("Enter coordinate point B in format x,y,z ");
+PrintData("Length beetwen points ", CalculateLength(pointA[0],pointA[1],pointA[2],pointB[0],pointB[1],pointB[2]));
+
+//method read user's data
+int[] ReadData(string message)
+{
+    Console.WriteLine(message);
+    string outputString = Console.ReadLine()??"0";
+    char[] coordString = outputString.ToCharArray();
+    int[] coordinate = new int[3];
+    coordinate[0] = ((int)coordString[0])-48;
+    coordinate[1] = ((int)coordString[2])-48;
+    coordinate[2] = ((int)coordString[4])-48;
+    return coordinate;
+}
+// method calculate result
+double CalculateLength(double x1,double y1,double z1,double x2,double y2, double z2)
+ {
+    double result = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+    return result;
+ }
+// method print result
+void PrintData(string msg, double len)
+{
+    Console.WriteLine(msg+len);
+}
