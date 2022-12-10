d061f31bd41194b5a703da610984e4df2b004336
whistle87 <i379@yandex.ru>
2022-12-10 17:20:06 +0300

    add task 24

---
+++ Sem4Taks24/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+int VariantSumSimple(int numA)
+{
+    int sumOfNum = 0;
+    for(int i=1; i<=numA; i++)
+    {
+        sumOfNum+=i;
+    }
+    return sumOfNum;
+}
+int VariantSumGause(int numA)
+{
+    int sumOfNum = 0;
+    sumOfNum = ((1+numA)*numA)/2;
+    return sumOfNum;
+}
+
+int numberA = ReadData("Enter a number A: ");
+DateTime d1 = DateTime.Now;
+int res1 = VariantSumSimple(numberA);
+Console.WriteLine(DateTime.Now - d1);
+DateTime d2 = DateTime.Now;
+int res2 = VariantSumGause(numberA);
+Console.WriteLine(DateTime.Now - d2);
+PrintResult("The amount of numbers from 1 to A is " + res1); 
