b71e6deedb57c7d1f52e0259f7999b1bf29199e4
whistle87 <i379@yandex.ru>
2022-12-10 17:43:31 +0300

    add task 26

---
+++ Sem4Task26/Program.cs
@@ -0,0 +1,45 @@
+﻿int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int VariantLog(int num)
+{
+    int count = (int)Math.Log10(num)+1;
+    return count;
+}
+int VariantString(int num)
+{
+    int count = num.ToString().Length;
+    return count;
+}
+int VariantCycle(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        count ++;
+        num = num / 10;
+    }
+    return count;
+}
+
+int number = ReadData("Enter a number ");
+DateTime d1 = DateTime.Now;
+int res1 = VariantLog(number);
+PrintResult($"There are {res1} digits in number");
+Console.WriteLine(DateTime.Now - d1 + " log");
+DateTime d2 = DateTime.Now;
+int res2 = VariantString(number);
+PrintResult($"There are {res2} digits in number");
+Console.WriteLine(DateTime.Now - d2 + " string");
+DateTime d3 = DateTime.Now;
+int res3 = VariantCycle(number);
+PrintResult($"There are {res3} digits in number");
+Console.WriteLine(DateTime.Now - d3 + " cycle");
