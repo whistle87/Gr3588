b96a4d89bf37bd9daca76df0dfaf7f6ae9f7713b
whistle87 <i379@yandex.ru>
2022-12-10 21:12:36 +0300

    add task 30

---
+++ Sem4task30/Program.cs
@@ -0,0 +1,30 @@
+﻿//solution
+int number = ReadData("Enter a length of array: ");
+int[] array = GenArray(number);
+PrintArray(array);
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+int[] GenArray(int len)
+{
+    int[] outArray = new int[len];
+    for (int i=0; i <len; i++)
+    {
+        outArray[i] = new Random().Next(0,2);
+    }
+    return  outArray;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i=0;i<array.Length-1;i++)
+    {
+        Console.Write(array[i]+",");
+    }
+    Console.Write(array[array.Length-1]+"]");
+}
