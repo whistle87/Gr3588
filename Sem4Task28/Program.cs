ef17594321eefa78f05d494d5d781cce0b65e61d
whistle87 <i379@yandex.ru>
2022-12-10 18:01:41 +0300

    add task 28

---
+++ Sem4Task28/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+BigInteger CalcFactor(int num)
+{
+    BigInteger count = 1;
+    for (int i=1; i<=num;i++)
+    {
+        count *=i;
+    }
+    return count;
+}
+
+int number = ReadData("Enter a number: ");
+BigInteger fact = CalcFactor(number);
+PrintResult("factorial is " + fact);
