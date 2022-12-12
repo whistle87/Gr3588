987f49a8a6ca6a5fdcaa59a35d98d2be5f2ad4df
whistle87 <i379@yandex.ru>
2022-12-12 15:28:42 +0300

    Add additional task

---
+++ Sem4TaskAdditional/Program.cs
@@ -0,0 +1,25 @@
+﻿//solutiuon
+string inputLine = ReadData("Enter a list of names: ");
+string[] names = SplitNames(inputLine);
+int randomNum = new Random().Next(0,names.Length);
+PrintResult(names[randomNum]);
+
+//method crate names array
+string[] SplitNames(string line)
+{
+    string[] names = line.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+    return names;
+}
+//method read data fron console
+string ReadData(string message)
+{
+    Console.WriteLine(message);
+    string line = Console.ReadLine()??"0";
+    return line;
+}
+//method print result
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
