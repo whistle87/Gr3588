0484c8c0c1a8329f97a391ece3c2580aa94371cf
whistle87 <i379@yandex.ru>
2022-12-20 17:14:44 +0300

    add task 40

---
+++ Sem6Task40/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int a = ReadData("Input a: ");
+int b = ReadData("Input a: ");
+int c = ReadData("Input a: ");
+PrintResult(TriangleTest(a,b,c));
+
+bool TriangleTest(int a, int b, int c)
+{
+    return (a+b>c&&b+c>a&&a+c>b);
+}
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int inputNum = int.Parse(Console.ReadLine()??"0");
+    return inputNum;
+}
+
+void PrintResult(bool res)
+{
+    if(res)
+    {
+        Console.Write("It can be so triangle");
+    }
+    else
+    {
+        Console.Write("It can't be so triangle");
+    }
+}
