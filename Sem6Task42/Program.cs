daf5c600b8cd417a0db532143f038759fea4a7d2
whistle87 <i379@yandex.ru>
2022-12-20 17:14:57 +0300

    add task 42

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int outputNum = ReadData("Enter a number");
+PrintResult("Bin number is "+DecToBin(outputNum));
+PrintResult("Bin number2 is "+DecToBinNativ(outputNum));
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int inputNum = int.Parse(Console.ReadLine()??"0");
+    return inputNum;
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+string DecToBin(int num)
+{
+    string outBin= String.Empty;
+    while(num > 0)
+    {
+        outBin = (num%2)+outBin;
+        num = num/2;       
+    }
+    return outBin;
+}
+
+string DecToBinNativ(int num)
+{
+    return Convert.ToString(num,2);
+}
