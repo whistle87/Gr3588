ff84da45f0fdb920c674c3c57895fca8b5dda63b
whistle87 <i379@yandex.ru>
2022-12-21 15:03:17 +0300

    add task 44

---
+++ Sem6Task44/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int number = ReadData("Enter a number ");
+PrintResult("Fibbonachi: "+Fibbanachi(number));
+
+string Fibbanachi(int num)
+{
+    string outLine = "0 1";
+    int buf = 0;
+    int last = 1;
+    int first = 0;
+    for(int i=2; i<num; i++)
+    {
+        outLine = outLine + " " + (last+first).ToString();
+        buf = last;
+        last = last+first;
+        first = buf;
+    }
+    return outLine;
+}
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int inputNum = int.Parse(Console.ReadLine()??"0");
+    return inputNum;
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
