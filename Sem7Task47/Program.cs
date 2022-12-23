57d8a13e8b271c5cca39f0a358868fa392d283c8
whistle87 <i379@yandex.ru>
2022-12-23 16:33:35 +0300

    Add task 47

---
+++ Sem7Task47/Program.cs
@@ -0,0 +1,67 @@
+﻿
+void Print2DArrayColor(double[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]
+{
+    ConsoleColor.Black,
+    ConsoleColor.Blue,
+    ConsoleColor.Cyan,
+    ConsoleColor.DarkBlue,
+    ConsoleColor.DarkCyan,
+    ConsoleColor.DarkGray,
+    ConsoleColor.DarkGreen,
+    ConsoleColor.DarkMagenta,
+    ConsoleColor.DarkRed,
+    ConsoleColor.DarkYellow,
+    ConsoleColor.Gray,
+    ConsoleColor.Green,
+    ConsoleColor.Magenta,
+    ConsoleColor.Red,
+    ConsoleColor.White,
+    ConsoleColor.Yellow
+};
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            string numString = matrix[i,j].ToString();
+            for (int elem = 0; elem<numString.Length; elem++)
+            {   
+                Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+                Console.Write(numString[elem]);
+            }
+            Console.Write("           ".Substring(matrix[i,j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int inputNum = int.Parse(Console.ReadLine() ?? "0");
+    return inputNum;
+}
+
+double[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    double[,] array2D = new double[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder)+rand.NextDouble();
+            array2D[i,j] = Math.Round(array2D[i,j],3);
+        }
+    }
+
+    return array2D;
+}
+
+//
+int row = ReadData("Enter an amount of row ");
+int column = ReadData("Enter an amount of column ");
+double[,] arr2D = Fill2DArray(row, column, 1, 50);
+Print2DArrayColor(arr2D);
