30530ae571fc48b922ead1def42d9a65e6d06e55
whistle87 <i379@yandex.ru>
2022-12-23 15:24:53 +0300

    Add task 48

---
+++ Sem7Task48/Program.cs
@@ -0,0 +1,59 @@
+﻿int row = ReadData("Enter an amount of row ");
+int column = ReadData("Enter an amount of column ");
+int[,] arr2D = Fill2DIndexed(row, column);
+ConsoleColor[] col = new ConsoleColor[]
+{
+    ConsoleColor.Black,
+    ConsoleColor.Blue,
+    ConsoleColor.Cyan,
+    ConsoleColor.DarkBlue,
+    ConsoleColor.DarkCyan,
+    ConsoleColor.DarkGray,
+    ConsoleColor.DarkGreen,
+    ConsoleColor.DarkMagenta,
+    ConsoleColor.DarkRed,
+    ConsoleColor.DarkYellow,
+    ConsoleColor.Gray,
+    ConsoleColor.Green,
+    ConsoleColor.Magenta,
+    ConsoleColor.Red,
+    ConsoleColor.White,
+    ConsoleColor.Yellow
+};
+
+Print2DArrayColor(arr2D);
+
+void Print2DArrayColor(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0, 16)];
+            Console.Write(matrix[i, j]+"   ".Substring(matrix[i,j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int inputNum = int.Parse(Console.ReadLine() ?? "0");
+    return inputNum;
+}
+
+int[,] Fill2DIndexed(int countRow, int countColumn)
+{
+    int[,] array2D = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = i + j;
+        }
+    }
+
+    return array2D;
+}
