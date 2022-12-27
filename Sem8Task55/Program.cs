7b0e1491a1003bd4b2fd31e3cb8c6fac6d4b7ed5
whistle87 <i379@yandex.ru>
2022-12-27 14:18:05 +0300

    Add task 55

---
+++ Sem8Task55/Program.cs
@@ -0,0 +1,93 @@
+﻿// method read data from console
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int inputNum = int.Parse(Console.ReadLine() ?? "0");
+    return inputNum;
+}
+
+// method fill array with random int numbers
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder);
+        }
+    }
+    return array2D;
+}
+void Print2DArrayColor(int[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]
+    {
+        ConsoleColor.Black,
+        ConsoleColor.Blue,
+        ConsoleColor.Cyan,
+        ConsoleColor.DarkBlue,
+        ConsoleColor.DarkCyan,
+        ConsoleColor.DarkGray,
+        ConsoleColor.DarkGreen,
+        ConsoleColor.DarkMagenta,
+        ConsoleColor.DarkRed,
+        ConsoleColor.DarkYellow,
+        ConsoleColor.Gray,
+        ConsoleColor.Green,
+        ConsoleColor.Magenta,
+        ConsoleColor.Red,
+        ConsoleColor.White,
+        ConsoleColor.Yellow
+    };
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[matrix[i,j]%15];
+            Console.Write(matrix[i, j] + "  ".Substring(matrix[i, j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+bool TTest(int[,] matrix)
+{
+    if (matrix.GetLength(0) == matrix.GetLength(1))
+        return true;
+    else
+    {
+        Console.WriteLine("This matrix can't be tranponsed");
+        return false;
+    }
+}
+
+//method
+void TUpdate(int[,] matr)
+{
+    int temp = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = i + 1; j < matr.GetLength(1); j++)
+        {
+            temp = matr[i, j];
+            matr[i, j] = matr[j, i];
+            matr[j, i] = temp;
+        }
+    }
+}
+
+int row = ReadData("Enter am amount of row ");
+int column = ReadData("Enter am column of row ");
+
+int[,] arr2D = Fill2DArray(row, column, 1, 10);
+Print2DArrayColor(arr2D);
+if (TTest(arr2D))
+{
+    TUpdate(arr2D);
+    Console.WriteLine();
+    Print2DArrayColor(arr2D);
+}
+// method check
