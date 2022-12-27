00010f4eacccdb176562dc44338fe9b7fb93a619
whistle87 <i379@yandex.ru>
2022-12-27 14:18:16 +0300

    Add task 57

---
+++ Sem8Task57/Program.cs
@@ -0,0 +1,89 @@
+﻿// method read data from console
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int inputNum = int.Parse(Console.ReadLine() ?? "0");
+    return inputNum;
+}
+
+// method fill array with random int numbers
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder);
+        }
+    }
+    return array2D;
+}
+
+void Print2DArrayColor(int[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]
+    {
+        ConsoleColor.Black,
+        ConsoleColor.Blue,
+        ConsoleColor.Cyan,
+        ConsoleColor.DarkBlue,
+        ConsoleColor.DarkCyan,
+        ConsoleColor.DarkGray,
+        ConsoleColor.DarkGreen,
+        ConsoleColor.DarkMagenta,
+        ConsoleColor.DarkRed,
+        ConsoleColor.DarkYellow,
+        ConsoleColor.Gray,
+        ConsoleColor.Green,
+        ConsoleColor.Magenta,
+        ConsoleColor.Red,
+        ConsoleColor.White,
+        ConsoleColor.Yellow
+    };
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[matrix[i, j] % 15];
+            Console.Write(matrix[i, j] + "    ".Substring(matrix[i, j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+//method print array
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ",");
+    }
+    Console.Write(array[array.Length - 1] + "]");
+}
+
+//
+int[] FillFreqDic(int[,] arr)
+{
+    int[] freqDic = new int[10];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            freqDic[arr[i, j]]++;
+        }
+    }
+    return freqDic;
+}
+
+////////
+int row = ReadData("Enter am amount of row ");
+int column = ReadData("Enter am column of row ");
+int[,] arr2D = Fill2DArray(row, column, 0, 10);
+Print2DArrayColor(arr2D);
+Console.WriteLine();
+int[] freqDic = FillFreqDic(arr2D);
+PrintArray(freqDic);
