ef3fe824b88af8a03fdf823f28271a634c230572
whistle87 <i379@yandex.ru>
2022-12-28 17:23:09 +0300

    Add task 67

---
+++ Sem9Task67/Program.cs
@@ -0,0 +1,23 @@
+﻿//method read data from console
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int inputNum = int.Parse(Console.ReadLine() ?? "0");
+    return inputNum;
+}
+
+//method print result to console
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int RecSumDigit(int number)
+{
+    if (number <= 10 ) return number;
+    else return  number%10 + RecSumDigit(number/10); 
+}
+//
+int inputNumber = ReadData("Enter number: ");
+int result = RecSumDigit(inputNumber);
+PrintResult("Sum of digit " + result);
