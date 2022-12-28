8b83c70fef8d0ed574b761df1d74cc3fec676188
whistle87 <i379@yandex.ru>
2022-12-29 00:09:13 +0300

    Add task 64

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,26 @@
+﻿//method read data from console
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int inputNum = int.Parse(Console.ReadLine() ?? "0");
+    return inputNum;
+}
+
+//method print result sequence to console
+void PrintSequence(int num)
+{    
+    if (num <= 1)
+    {
+        Console.Write(num);
+        return;
+    }
+    else
+    {
+        Console.Write(num+",");
+        PrintSequence(num - 1);
+    }
+}
+//solution
+int number = ReadData("Enter number N: ");
+PrintSequence(number);
+
