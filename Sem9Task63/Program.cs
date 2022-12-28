552d3d343ea1ccc62466b026dd573575de77cb04
whistle87 <i379@yandex.ru>
2022-12-28 17:22:42 +0300

    Add task 63

---
+++ Sem9Task63/Program.cs
@@ -0,0 +1,30 @@
+﻿//method read data from console
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int inputNum = int.Parse(Console.ReadLine() ?? "0");
+    return inputNum;
+}
+
+//method print result to console
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+string LineGenLen(int num)
+{
+    if (num <= 0)
+    {
+        return string.Empty;
+    }
+    else
+    {
+        string outLine = num + " "+ LineGenLen(num - 1);
+        return outLine;
+    }
+}
+//
+int number = ReadData("Enter number N: ");
+string resultLine = LineGenLen(number);
+PrintResult(resultLine);
